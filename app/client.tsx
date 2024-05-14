05368016cbbc7e4c63cf09e7ed0bec1778304b69
Tanner Linsley <tannerlinsley@gmail.com>
2024-05-14 00:41:52 -0600

    start/vinxi attempt

--- app/client.tsx
+++ app/client.tsx
@@ -1,13 +1,16 @@
-/// <reference types="vinxi/types/client" />
-import * as React from 'react'
 import { hydrateRoot } from 'react-dom/client'
-import 'vinxi/client'
-
-import { createRouter } from './router'
 import { StartClient } from '@tanstack/start'
+import { createRouter } from './router'
 import './utils/sentry'
+import { startTransition } from 'react'
 
 const router = createRouter()
 
 // hydrateRoot(document, <StartClient router={router} />)
-hydrateRoot(document.getElementById('root')!, <StartClient router={router} />)
+// startTransition(() => {
+hydrateRoot(document.getElementById('root')!, <StartClient router={router} />, {
+  onRecoverableError(error, errorInfo) {
+    console.error(error, errorInfo)
+  },
+})
+// })
--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -47,19 +47,19 @@ export const Route = createRootRouteWithContext<{
     {
       rel: 'apple-touch-icon',
       sizes: '180x180',
-      href: '/favicons/apple-touch-icon.png',
+      href: '/apple-touch-icon.png',
     },
     {
       rel: 'icon',
       type: 'image/png',
       sizes: '32x32',
-      href: '/favicons/favicon-32x32.png',
+      href: '/favicon-32x32.png',
     },
     {
       rel: 'icon',
       type: 'image/png',
       sizes: '16x16',
-      href: '/favicons/favicon-16x16.png',
+      href: '/favicon-16x16.png',
     },
     { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
     { rel: 'icon', href: '/favicon.ico' },
